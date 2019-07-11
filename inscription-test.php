981fe235012fd92b4681ed9a719c070cfbe608cb
UlyssePerret <ulysseperret@orange.fr>
2019-07-11 16:48:29 +0200

    Add files via upload

---
+++ inscription-test.php
@@ -0,0 +1,38 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Inscris-toi !</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        <form method="post">
+
+          <input type="text" name="pseudo" id="pseudo" class="form-control" placeholder="pseudo"><br>
+
+          <input type="text" name="mdp" id="mdp" class="form-control" placeholder="mot de passe"><br>
+
+          <input type="text" name="nom" id="nom" class="form-control" placeholder="nom"><br>
+
+          <input type="text" name="prenom" id="prenom" class="form-control" placeholder="prenom"><br>
+
+          <input type="text" name="email" id="email" class="form-control" placeholder="email"><br>
+
+          <input type="text" name="ville" id="ville" class="form-control" placeholder="telephone"><br>
+
+      <select id="select" style="width:100%; text-align: center;">
+        <option value="h">homme</option>
+        <option value="f">femme</option>
+      </select><br><br>
+
+      </form>
+      </div>
+      <div class="modal-footer">
+        <input type="submit" class="btn btn-success"  style="width: 100%; text-align: center;" value="Inscription">
+      </div>
+    </div>
+  </div>
+</div>
